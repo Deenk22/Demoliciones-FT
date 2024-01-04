adbe453a9dee8edca902465c55079f411cee285c
Deenk22 <129082676+Deenk22@users.noreply.github.com>
2024-01-04 19:18:27 +0100

    Deploy

--- Front-End/src/utils/mailSchema.js
+++ Front-End/src/utils/mailSchema.js
@@ -1,4 +1,4 @@
-import {z} from "zod";
+import { z } from 'zod';
 
 const phoneRegex = new RegExp(
   /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
@@ -6,10 +6,10 @@ const phoneRegex = new RegExp(
 
 export const mailSchema = z.object({
   name: z.string(),
-  email: z.string().email("Introduce un email válido"),
+  email: z.string().email('Introduce un email válido'),
   phone: z
     .string()
-    .max(15, "Solo 15 dígitos como máximo")
-    .regex(phoneRegex, "Introduce un número válido"),
+    .max(15, 'Solo 14 dígitos como máximo')
+    .regex(phoneRegex, 'Introduce un número válido'),
   mensaje: z.string(),
 });
